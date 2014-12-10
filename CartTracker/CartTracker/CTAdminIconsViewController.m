85efbf0cdc6ee83dbdbbfe3cdf8f4768c07289c7
andresruggiero <arugg003@fiu.edu>
2014-12-10 11:15:35 -0500

    Fixed the AdminIcon

--- CartTracker/CartTracker/CTAdminIconsViewController.m
+++ CartTracker/CartTracker/CTAdminIconsViewController.m
@@ -12,7 +12,6 @@
 #import "Constants.h"
 #import "CTDataListViewController.h"
 #import "CTLoginViewController.h"
-#import "CTStatisticsTableViewController.h"
 
 @interface CTAdminIconsViewController ()
 
@@ -59,8 +58,8 @@
             break;
         case STATISTICS:
         {
-            CTStatisticsTableViewController *statsController = [[CTStatisticsTableViewController alloc] init];
-            [self.navigationController pushViewController:statsController animated:YES];
+            //CTStatisticsTableViewController *statsController = [[CTStatisticsTableViewController alloc] init];
+            //[self.navigationController pushViewController:statsController animated:YES];
         }
             break;
         case LOG_OUT:
